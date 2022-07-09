a69e116f3133e6fd203a5322a56b0eaeee294177
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2022-07-09 11:01:59 -0500

    added my parent Overview JSX tag to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 
 import React from "react";
-
+import Overview from '/Users/lawrence/FEC-Team-Io/src/Overview/components/Overview.jsx';
 class App extends React.Component {
   render() {
     const { name } = this.props;
     return (
-      <>
+      <body>
         <h1>
-          Hello {name}
+          Hello{name}
         </h1>
-      </>
+        <div id='overview'>
+        <Overview/>
+      </div>
+      </body>
     );
   }
 }
