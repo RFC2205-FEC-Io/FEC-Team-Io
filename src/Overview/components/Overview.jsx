8d2e4e7b828f849e6e46cb0fbd81b8c6a77cb169
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2022-07-10 18:03:23 -0500

    added request handler to styleslector component, style names sucessfully render onto the page

--- src/Overview/components/Overview.jsx
+++ src/Overview/components/Overview.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
-// import ReactDom from 'react-dom';
 import ImageGallery from './ImageGallery.jsx';
 import ProductInfo from './ProductInfo.jsx';
 import StyleSelector from './StyleSelector.jsx';
 import AddToCart from './AddToCart.jsx';
+const axios = require('axios');
 class Overview extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
-
+      products: []
     }
   }
+
+  componentDidMount () {
+    // console.log('Overview, mounted');
+    const APItoken = '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';
+    axios({
+      method: 'get',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/products',
+      headers: {
+        'Authorization': APItoken,
+      }
+    })
+    .then((res) => {
+      // console.log('GET sent, products retreived!:', res.data);
+      this.setState({
+        products: res.data
+      })
+    })
+    .catch((err) => {
+      throw err;
+    });
+  }
+
   render (props) {
     return (
       <div id='overview'>
--- src/Overview/components/StyleSelector.jsx
+++ src/Overview/components/StyleSelector.jsx
@@ -1,16 +1,44 @@
 import React from 'react';
-
+const axios = require('axios');
 class StyleSelector extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
-
+      styles: []
     }
+    // this.renderStyles = this.renderStyles.bind(this);
   }
+  componentDidMount() {
+    console.log('StyleSelector, mounted');
+    const APItoken = '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';
+    axios({
+      method: 'get',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/products/66642/styles',
+      headers: {
+        'Authorization': APItoken,
+      }
+    })
+    .then((res) => {
+      console.log('GET sent, products retreived!:', res.data.results);
+      this.setState({
+        styles: res.data.results
+      });
+    })
+    .catch((err) => {
+      throw err;
+    });
+  }
+
+
   render (props) {
     return (
       <div id='image-gallery'>
-        Product Info
+        <h2 onClick={this.renderStyles}>Style-Selector</h2>
+        {console.log('this.state.styles.results:', this.state.styles)}
+        {this.state.styles.map ((style) => {
+          console.log('this is prouct in the map function:', style.name);
+          return <div>{style.name}</div>;
+        })}
       </div>
     );
   }
