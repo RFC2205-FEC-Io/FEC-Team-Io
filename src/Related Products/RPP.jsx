108e4e7cf43405baadff991337060c620ad425db
Monica Cupp <monicaycupp@gmail.com>
2022-07-11 16:17:45 -0500

    Completes static rating stars mini-component

--- src/Related Products/RPP.jsx
+++ src/Related Products/RPP.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Components/RelatedProductStyles.css';
 import InteractiveStarRating from './Components/InteractiveStarRating.jsx';
+import StaticStarRating from './Components/StaticStarRating.jsx';
 
 function RPP () {
   return (
     <div className="RPP">
-      <InteractiveStarRating />
+      <StaticStarRating />
     </div>
   );
 }
