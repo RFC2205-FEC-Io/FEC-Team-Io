4ad01fc3156e37aac3abe329e714492a100b22bc
Monica Cupp <monicaycupp@gmail.com>
2022-07-10 19:45:29 -0500

    Completes interactive star component

---
+++ src/Related Products/RPP.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Components/RelatedProductStyles.css';
+import InteractiveStarRating from './Components/InteractiveStarRating.jsx';
+
+function RPP () {
+  return (
+    <div className="RPP">
+      <InteractiveStarRating />
+    </div>
+  );
+}
+
+export default RPP;
