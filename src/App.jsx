1054f5f1547c3189e6bc51f6d192252df6de187f
Andrew <amw.infospecialist@gmail.com>
2022-07-12 14:57:37 -0500

    Test files started. API Working.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import Overview from '/Users/lawrence/FEC-Team-Io/src/Overview/components/Overview.jsx';
+import Overview from './Overview/components/Overview.jsx';
 import ReviewsApp from "./Reviews/ReviewsApp.jsx"
 import RPP from "./Related Products/RPP.jsx";
 
@@ -9,9 +9,9 @@ class App extends React.Component {
     const { name } = this.props;
     return (
       <div>
-        <Overview/>
-        <ReviewsApp />
+        {/* <Overview/> */}
         <RPP />
+        <ReviewsApp />
       </div>
     );
   }
