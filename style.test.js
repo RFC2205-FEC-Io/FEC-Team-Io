065674da58a7751391949190555609e5bcc873c1
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2022-07-12 15:03:51 -0500

    added more modules to jest

--- style.test.js
+++ style.test.js
@@ -1,21 +1,12 @@
 // import 'react-native';
 import React from 'react';
 import {shallow} from 'enzyme';
-import PrductInfo from '../../components/ProductInfo.jsx';
-
+import ProductInfo from '/Users/lawrence/FEC-Team-Io/src/Overview/components/ProductInfo.jsx';
+import Adapter from 'enzyme-adapter-react-15';
 describe('first snapshot test', () => {
   it ('renders the AddToCart component correctly', () => {
-    const wrapper = shallow(<AddToCart/>);
+    const wrapper = shallow(<ProductInfo/>);
     expect(wrapper).toMatchSnapshot();
   });
 });
 
-// import renderer from 'react-test-renderer';
-// import PrductInfo from '../../components/ProductInfo.jsx';
-
-// it('renders correctly', () => {
-//   const tree = renderer
-//     .create(<AddToCart/>)
-//     .toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
