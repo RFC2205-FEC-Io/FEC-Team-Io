eaab3add045ec9cf1f5d7b065c2e18f18046327f
Monica Cupp <monicaycupp@gmail.com>
2022-07-12 22:15:48 -0500

    Begins connecting API data through server

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
+const axios = require("axios");
 const cors = require("cors");
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+console.log('API_KEY: ', API_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -14,3 +19,38 @@ app.get("/*", (req, res) => {
 app.listen(3000, () => {
   console.log("App running on http://localhost:3000");
 });
+
+//----------------API Requests for Related Products---------------//
+
+/* Handler for related product IDs requests*/
+app.get('/:product_id/related', (req, res) => {
+  console.log('Incoming request: ', req);
+  getRelatedProductIds()
+  .then (function (idArray) {
+    console.log('Related product id numbers acquired!')
+    return res.json(idArray)
+  })
+  .catch (function (err) {
+    console.log('Uh-oh! API request error: ' + err);
+    return res.sendStatus(500);
+  })
+})
+
+/*Request function for related product IDs requests*/
+getRelatedProductIds = (productID) => {
+  var config = {
+    method: 'get',
+    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/products/'${productID}'/related`,
+    headers: {
+      'Authorization': API_KEY
+    }
+  };
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+};
+
--- src/Related Products/RPP.jsx
+++ src/Related Products/RPP.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import './Components/RelatedProductStyles.css';
 import InteractiveStarRating from './Components/InteractiveStarRating.jsx';
 import StaticStarRating from './Components/StaticStarRating.jsx';
@@ -11,16 +12,31 @@ class RPP extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      randomInfo: []
+      currentId: 66645,
+      relatedProductIds: []
     }
     this.XIconButtonClickHandler = this.XIconButtonClickHandler.bind(this);
+    this.getRelatedProductIds = this.getRelatedProductIds.bind(this);
   }
-
-
+  componentDidMount() {
+    this.getRelatedProductIds(this.state.currentId);
+  }
  XIconButtonClickHandler (event) {
   console.log("X Icon Button was clicked.")
  }
 
+getRelatedProductIds(currentId) {
+  axios.get('/products/:product_id/related', {params: {product_id: this.state.currentID}})
+  .then(response => {
+    console.log('response: ', response)
+    this.setState({
+      relatedProductIds: response.data
+    })
+  })
+  .catch(err => {
+    console.log('getRelatedProductIds ERROR: ', err)
+  });
+}
 
 
   render () {
