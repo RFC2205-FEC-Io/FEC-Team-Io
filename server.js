34327f96e33b93f96ced4f32c9a0bede7e26ac64
Monica Cupp <monicaycupp@gmail.com>
2022-07-13 09:48:16 -0500

    Moved things to ignore

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
-});
 
 app.listen(3000, () => {
   console.log("App running on http://localhost:3000");
