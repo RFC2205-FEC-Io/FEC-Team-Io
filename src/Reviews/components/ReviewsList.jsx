eff9e27ac78138139e28a2f8fcc57212bcf9e37c
Andrew Wallace <amw.infospecialist@gmail.com>
2022-07-14 20:40:55 -0500

    Ratings and reviews PR 7/14/22 (#18)
    * ReviewList and ReviewTile mostly functional
    
    * ReviewTile Quick edit
    
    * Added bundle.js
    
    * Added gitIgnore
    
    * Test files started. API Working.
    
    * Delete .env
    
    * Server.js change before Development Pull
    
    * Page completely functional after merge party
    
    * ReviewsList not populating. StarRatings components redone.

--- src/Reviews/components/ReviewsList.jsx
+++ src/Reviews/components/ReviewsList.jsx
@@ -10,54 +10,78 @@ class ReviewsList extends React.Component {
     this.state = {
       reviewArray: [],
       productID: 66642,
+      displayCount: 2,
+      displayList: [],
     }
   };
 
-  componentDidMount() {
-    this.getReviews()
+  async componentDidMount() {
+    const reviewDB = await getReviews();
+    console.log("reviewArray after GET: ", this.state.reviewArray)
   };
 
-  getReviews = () => {
-    axios.get(`/reviews/?product_id=${this.state.productID}&count=100`)
-    // axios.get('/reviews', {params: {product_id: this.state.productID}})
-    .then(res => {
-      // console.log('res.data: ', res.data);
-      this.setState({reviewArray: res.data.results});
-    })
-    .catch(err => {
-      alert('Error, no reviews to post');
-    })
-  };
-
-  // reviewTiles = (this.state.reviewArray) => {
-  //   return 'xyz';
+  //the code block below should map from reviewArray in state to displayList per current displayCount. Having difficulty figuring out where to put it.
+  // for (var i = 0; i < this.state.displayCount; i++) {
+  //   console.log("reviewArray[i]: ", this.state.reviewArray[i])
+  //   this.state.displayList.push(this.state.reviewArray[i]);
   // }
+  // console.log('displayList: ', this.state.displayList)
+
+  getReviews() {
+    return axios.get(`/reviews/?product_id=${this.state.productID}&count=100`)
+      .then(res => {
+        // console.log('res.data: ', res.data);
+        this.setState({ reviewArray: res.data.results });
+      })
+      .catch(err => {
+        alert('Error, no reviews to post');
+      })
+  };
+
+  displayCount = () => {
+    this.setState({
+      displayCount: this.state.displayCount + 2
+    })
+  }
+
+  mapDisplayList = () => {
+    for (var i = 0; i < this.state.displayCount; i++) {
+      console.log("reviewArray[i]: ", this.state.reviewArray[i])
+      this.state.displayList.push(this.state.reviewArray[i]);
+    }
+    console.log('displayList: ', this.state.displayList)
+  }
 
   render() {
+    this.mapDisplayList()
     return (
       <div>
         <p id="reviewListHeader">ReviewsList Here</p>
         <ul id="reviewTileList">
-          {this.state.reviewArray.map(review =>
+          {/* {this.mapDisplayList} */}
+          {this.state.displayList.map(review =>
             // return (
-              <div>
-                <ReviewTile
-                  key={review.review_ID}
-                  rating={review.rating}
-                  summary={review.summary}
-                  recommend={review.recommend}
-                  response={review.response}
-                  body={review.body}
-                  date={review.date}
-                  reviewerName={review.reviewer_name}
-                  helpfulness={review.helpfulness}
-                  photos={review.photos}
-                  />
-              </div>
+            <div>
+              <ReviewTile
+                key={review.review_id}
+                rating={review.rating}
+                summary={review.summary}
+                recommend={review.recommend}
+                response={review.response}
+                body={review.body}
+                date={review.date}
+                reviewerName={review.reviewer_name}
+                helpfulness={review.helpfulness}
+                photos={review.photos}
+              />
+            </div>
             // )
-          )}
-          </ul>
+          )
+          }
+        </ul>
         {/* <button id="ReviewFormButton" onClick={}>Add A Review +</button> */}
+        <button id="ReviewDisplayIncrease" onClick={this.displayCount}>See More Reviews</button><br></br>
+        <button id="reviewAddForm" onClick="someFunctionHere">Add a Review</button>
       </div>
     );
   }
