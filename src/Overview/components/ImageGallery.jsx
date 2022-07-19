51ac76eafc3b6857d35bafcb4c53239bfa2441f2
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2022-07-19 15:44:21 -0500

    3:44

--- src/Overview/components/ImageGallery.jsx
+++ src/Overview/components/ImageGallery.jsx
@@ -77,42 +77,58 @@ const ImageGallery = ({images, clickedThumb, thumbnailClicked, galleryIMG, toggl
         );
       });
     } else {
-      // return images.map((image) => {
-      //   return (
-      //     <div>
-      //       <img
-      //       id ='gallery'src={image.thumbnail_url}
-      //       onClick={()=> {setBackgroundImage(event, image.url); toggleImages()}}
-      //       />
-      //     </div>
-      //   );
-      // });
-      return (
-        <Carousel>
-          {/* {images.map((image) => {
+      return images.map((image) => {
         return (
-          <Carousel.item>
+          <div>
             <img
             id ='gallery'src={image.thumbnail_url}
             onClick={()=> {setBackgroundImage(event, image.url); toggleImages()}}
             />
-          </Carousel.item>
+          </div>
         );
-      } */}
-        <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-        </Carousel>
-      );
+      });
     }
+    // return (
+      //   <Carousel>
+      //     <Carousel.Item>
+      //       <img
+      //         className="d-block w-100"
+      //         src="holder.js/800x400?text=First slide&bg=373940"
+      //         alt="First slide"
+      //       />
+      //       <Carousel.Caption>
+      //         <h3>First slide label</h3>
+      //         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      //       </Carousel.Caption>
+      //     </Carousel.Item>
+      //     <Carousel.Item>
+      //       <img
+      //         className="d-block w-100"
+      //         src="holder.js/800x400?text=Second slide&bg=282c34"
+      //         alt="Second slide"
+      //       />
+
+      //       <Carousel.Caption>
+      //         <h3>Second slide label</h3>
+      //         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      //       </Carousel.Caption>
+      //     </Carousel.Item>
+      //     <Carousel.Item>
+      //       <img
+      //         className="d-block w-100"
+      //         src="holder.js/800x400?text=Third slide&bg=20232a"
+      //         alt="Third slide"
+      //       />
+
+      //       <Carousel.Caption>
+      //         <h3>Third slide label</h3>
+      //         <p>
+      //           Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+      //         </p>
+      //       </Carousel.Caption>
+      //     </Carousel.Item>
+      //   </Carousel>
+      // );
   }
 
   return (
