55f009d5ffb69b204b0d2a8d02388335d15ad3f6
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2022-07-22 12:27:58 -0500

    thumbnails without any images are now polulated with a placeholder

--- src/Overview/components/Style.jsx
+++ src/Overview/components/Style.jsx
@@ -3,9 +3,26 @@ import check_mark from '../../../dist/check_mark.png';
 const Style = ({styles, styleClick, clicked, name}) => {
   var addStyle = () => {
     var placeholder = '/Smiley%20Shades.png';
+      // ------------Sets the style gallery if there asre FEWER THAN 5 THUMNBAILS------------
     if (styles.length <= 4) {
       return styles.map ((style) => {
         if (style.name === name && clicked) {
+          console.log('style:', style);
+          // ------------IF THE THUMBNAIL URL IS NULL-----------
+          if (!style.photos[0].url) {
+            return (
+              <div
+                key={style.name}
+                id='style-img'
+                style={{backgroundImage: `url(${placeholder})`}}
+                onClick={(event, name, styleObj, originalPrice, salePrice, img) => {
+                  styleClick (event, style.name, style, style.original_price, style.sale_price, style.photos[0].url);
+                }}
+                >
+              <img class='check-mark 'src={check_mark}></img>
+              </div>
+            );
+          }
           return (
             <div
               key={style.name}
@@ -19,6 +36,20 @@ const Style = ({styles, styleClick, clicked, name}) => {
             </div>
             );
         } else {
+          // ------------IF THE THUMBNAIL URL IS NULL-----------
+          if (!style.photos[0].url) {
+            return (
+              <div
+                key={style.name}
+                id='style-img'
+                style={{backgroundImage: `url(${placeholder})`}}
+                onClick={(event, name, styleObj, originalPrice, salePrice, img) => {
+                  styleClick (event, style.name, style, style.original_price, style.sale_price, style.photos[0].url);
+                }}
+                >
+              </div>
+            );
+          }
         return (
           <div
             id='style-img'
@@ -33,14 +64,16 @@ const Style = ({styles, styleClick, clicked, name}) => {
       }
     })
   } else {
+        // ------------Sets the style gallery if there asre MORE THAN 5 THUMNBAILS------------
     var slicepoint = styles.length - 4;
     var styleThumbArr1 = styles.slice(0, 4);
-    var styleThumbArr2 = styles.slice(slicepoint, styles.length -1);
+    var styleThumbArr2 = styles.slice(-slicepoint, styles.length);
+    var count = 0;
     return (
     <div id='split-styles-thumb'>
       <div id='thumb1'>
-  {styleThumbArr1.map((style) => {
-    if (style.name === name && clicked) {
+      {styleThumbArr1.map((style) => {
+      if (style.name === name && clicked) {
       return (
         <div
         key={style.name}
@@ -69,8 +102,11 @@ const Style = ({styles, styleClick, clicked, name}) => {
   })}
       </div>
       <div id='thumb2'>
-      { styleThumbArr2.map((style) => {
-    if (style.name === name && clicked) {
+        {console.log('styleThumbArr2:', styleThumbArr2, 'slicepoint:', slicepoint)}
+      {styleThumbArr2.map((style) => {
+        count++;
+        console.log('count:', count);
+      if (style.name === name && clicked) {
       return (
         <div
         key={style.name}
@@ -110,18 +146,3 @@ const Style = ({styles, styleClick, clicked, name}) => {
 }
 
 export default Style;
-
-        // if (!style.photos.thumbnail_url) {
-        //   return (
-        //     <div
-        //       key={style.name}
-        //       id='style-img'
-        //       style={{backgroundImage: `url(${placeholder})`}}
-        //       onClick={(event, name, styleObj, originalPrice, salePrice, img) => {
-        //         styleClick (event, style.name, style, style.original_price, style.sale_price, placeholder);
-        //       }}
-        //       >
-        //     <img class='check-mark 'src={check_mark}></img>
-        //     </div>
-        //   );
-        // }
