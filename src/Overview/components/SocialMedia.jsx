46dfa99ef3c4fbae502b4d396ca220f6282a9670
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2022-07-22 14:39:54 -0500

    last minute changes

--- src/Overview/components/SocialMedia.jsx
+++ src/Overview/components/SocialMedia.jsx
@@ -3,13 +3,13 @@ const SocialMedia = () => {
   return (
     <div id='social-media'>
       <a href='https://www.facebook.com/'>
-        <img className = 'social-icon' id='facebook' src = 'http://localhost:3000/assets/social-media/icon-facebook.png'/>
+        <img className = 'social-icon' id='facebook' src = '/assets/social-media/icon-facebook.png'/>
       </a>
       <a href='https://www.instagram.com/'>
-        <img className = 'social-icon' id='instagram' src = 'http://localhost:3000/assets/social-media/icon-instagram.png'/>
+        <img className = 'social-icon' id='instagram' src = '/assets/social-media/icon-instagram.png'/>
       </a>
       <a href='https://www.pinterest.com/login/'>
-        <img className = 'social-icon' id='pinterest' src = 'http://localhost:3000/assets/social-media/icon-pinterest.png'/>
+        <img className = 'social-icon' id='pinterest' src = '/assets/social-media/icon-pinterest.png'/>
       </a>
     </div>
   );
